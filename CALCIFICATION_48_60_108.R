b697564a201cb1ccf016a5ba1a2d0d1ae2466fef
Ricardo Gore <rjhgore@gmail.com>
2022-10-04 20:13:57 -0700

    Update CALCIFICATION_48_60_108.R

--- CALCIFICATION_48_60_108.R
+++ CALCIFICATION_48_60_108.R
@@ -1,7 +1,6 @@
 
 # test reducition of dimensions from birefrigency an dbody size
 
-
 rm(list = ls());
 
 if(!is.null(dev.list())) dev.off()
@@ -16,8 +15,9 @@ library(factoextra)
 library("FactoMineR")
 
 set.seed(123)
+# select(hpf, pH, Index) %>%
 
-body_size <- read_rds(paste0(getwd(), '/length_width_dataset.rds')) %>% select(hpf, pH, Index) %>% 
+body_size <- read_rds(paste0(getwd(), '/length_width_dataset.rds')) %>%select(hpf, pH, Index) %>%
   filter(hpf != 24) %>% mutate(g = 'growth') %>% rename('measure' = 'Index')
 birefrigence <- read_rds(paste0(getwd(), '/birefrigence.rds')) %>% select(Area) %>% mutate(g = 'calcification') %>% rename('measure' = 'Area')
 
@@ -167,31 +167,53 @@ birefrigence %>%
   facet_grid(hpf ~ ntile , scales = 'free_y') +
   scale_color_manual("", values = pHpalette) 
  
+# bonafide nos quedamos con el quatile 2 ----
+# Interquantil test ----
+
+birefrigence %>% 
+  mutate(ntile = ntile(measure, 2)) %>%
+  filter(ntile > 1) %>%  select(-ntile) -> birefrigence
+    
+# mad: median absolute deviation (see ?MAD)
+rbind(birefrigence, body_size) %>%
+  group_by(pH, hpf, g) %>%
+  rstatix::get_summary_stats(measure, show = c('iqr' ,'mad')) %>%
+  ggplot(aes(y = iqr, x = pH, fill = pH)) +
+  geom_col()+
+  theme_classic(base_family = "GillSans", base_size = 12) +
+  # geom_errorbar(aes(ymin = q1, ymax = q3), width = 0.2) +
+  facet_wrap(hpf ~ g, scales = 'free_y', ncol = 2) +
+  scale_fill_manual("", values = pHpalette)
+  
+
 # birefrigence %>% tally()
 
 # rbind(birefrigence, body_size) %>%
 
 birefrigence %>% # birefrigence not grouped good!
-  mutate(ntile = ntile(measure, 2)) %>%
-  group_by(pH, hpf, ntile) %>% # tally()
+  # mutate(ntile = ntile(measure, 2)) %>%
+  group_by(pH, hpf) %>% # tally()
   sample_n(20, replace = TRUE) %>%
-  pivot_wider(names_from = hpf, values_from = measure, values_fn = list) %>%
-  unnest(cols = everything()) %>%
-  filter(ntile > 1) -> df 
+  pivot_wider(names_from = pH, values_from = measure, values_fn = list) %>%
+  unnest(cols = everything()) -> df1 
+  # filter(ntile > 1) %>% ungroup() %>% select(-ntile) -> df1 
 
 body_size %>% # birefrigence not grouped good!
   group_by(pH, hpf, g) %>% # tally()
   sample_n(50, replace = TRUE) %>%
-  pivot_wider(names_from = hpf, values_from = measure, values_fn = list) %>%
-  unnest(cols = everything()) -> df
+  pivot_wider(names_from = pH, values_from = measure, values_fn = list) %>%
+  unnest(cols = everything()) -> df2
 
+df <- rbind(df1, df2) # %>% mutate_if(is.double, log)
+
+df
 
 # "ward.D", "ward.D2", "single", "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC) or "centroid" (= UPGMC).
 
 cmds <- run_cmds(df, k = 3)
 
 cmds %>% 
-  ggplot(aes(X1,X2, group = cutree)) + # shape = as.factor(df$ntile))
+  ggplot(aes(X1,X2, group = cutree, shape = as.factor(df$g))) + 
   ggforce::geom_mark_ellipse(color = NA, fill = 'grey', alpha = 0.5) +
   # coord_fixed() +
   geom_point(size = 4, aes(color = g), alpha = 0.5) +
@@ -211,6 +233,8 @@ fit <- linear_reg() %>%
 
 # performance::check_model(fit)
 
+Anova(fit$fit)
+
 fit %>% tidy() %>% add_significance("p.value") %>%
   mutate(g = 'Growth') -> out
 
@@ -226,21 +250,164 @@ fit <- linear_reg() %>%
   set_engine("glm") %>% # log_mod
   fit(measure ~ pH, data = birefrigence)
 
+# fit <- linear_reg() %>%
+#   set_engine("glm") %>% # log_mod
+#   fit(measure ~ pH*g, data = rbind(birefrigence, body_size))
+
+Anova(fit$fit)
+
 fit %>% tidy() %>% add_significance("p.value") %>%
   mutate(g = 'Calcification') %>%
   rbind(out) %>%
   filter(grepl('pH', term)) %>%
   mutate(ymax = estimate+std.error, 
     ymin  = estimate-std.error) %>%
-  ggplot(aes(y = estimate, x = term)) +
-  facet_grid(g ~ .) +
-  geom_point() +
+  mutate(term = sub('pH','', term)) %>%
+  ggplot(aes(y = estimate, x = g, color = term, group = term)) +
+  geom_point(position = position_dodge2(width = 0.4)) +
   geom_hline(yintercept = 0, colour = "grey50", linetype = 2) +
   geom_errorbar(aes(ymin = ymin, ymax = ymax),
-    width = 0.15,size = 0.7, position = position_dodge(width = 0.7), color = 'black') +
-  geom_text(aes(x = term, y = std.error + 0.05, 
-    label= p.value.signif)) +
-  theme_classic(base_size = 16, base_family = "GillSans") 
+    width = 0.4,size = 0.7, 
+    # color = 'black',
+    position = position_dodge2(width = 0.4)) +
+  geom_text(aes(y = std.error + 0.05, 
+    label= p.value.signif), position = position_dodge2(width = 0.4)) +
+  theme_classic(base_size = 16, base_family = "GillSans") +
+  scale_color_manual(values = pHpalette) +
+  scale_fill_manual(values = pHpalette) +
+  labs(y = 'Estimate efect', x = '')
   
+# MANOVA -----
+library(tidymodels)
+
+fit <- linear_reg() %>%
+  set_engine("lm") %>% 
+  fit(measure ~ pH * hpf, data = body_size)
+
+performance::check_model(fit)
+
+Anova(fit$fit)
+
+# candisc canonical discriminant analysis (CDA) -----
+# install.packages("candisc")
+library(candisc)
+# ref: Near-future level of CO2-driven ocean acidification radically affects larval survival and development in the brittlestar Ophiothrix fragilis (dpont 2008)
+
+# Canonical discriminant analysis of the morphometric parameters Figure: used to separate the different pH treatments and time post fertiliza- tion. At Day 1, all the larvae from the 3 pH treatments are clustered together indi- cating that they share similar body proportions. At Day 4, larvae raised at pH 7.7 are discriminated from those growing at pH 8.1 (all days) and pH 7.9 (Day 4). Larvae raised at pH 7.7 possessed proportions that were never observed in those raised at normal pH.
+
+# Method: Canonical discriminant analysis was used to assess the impacts of pH and/or exposure time on mor- phometric parameters
+
+# Canonical discriminant analyses were performed on the morphometric parameters to assess individual variation within the treat- ments at Days 1 and 4 . At Day 1, all the larvae from the 3 pH treatments are clustered together indi- cating that they share similar body proportions. At Day 4, larvae raised at pH 7.7 are discriminated from those growing at pH 8.1 (all days) and pH 7.9 (Day 4). Larvae raised at pH 7.7 possessed proportions that were never observed in those raised at normal pH.
+
+# mod <- lm(cbind(`8`,`7.8`,`7.6`) ~ hpf, data=df1)
+
+# PCA ensena la metrica con mayor variacion, pero el analisis de discriminacion (lineal) ofrece una medida de separacion (u) entre dos grupos.. ie. LDA is like PCA but it focuses on maximizn the seperatibility among known categories (statquest)
+
+body_size %>% # birefrigence not grouped good!
+  group_by(pH, hpf, g) %>% # tally()
+  sample_n(50, replace = TRUE) %>%
+  pivot_wider(names_from = pH, values_from = measure, values_fn = list) %>%
+  unnest(cols = everything()) -> df
+
+mod <- lm(cbind(`8`,`7.8`,`7.6`) ~ hpf*g, data=df)
+
+# mod <- lm(cbind(`48`,`60`,`108`) ~ pH , data=df2)
+
+Anova(mod, test.statistic = 'Roy')
+Anova(update(mod, `7.6` ~ .))
+
+can <- candisc(mod, data = df, term = 'pH', ndim = "2")
+
+# plot(can, col = rev(pHpalette))
+
+# From this we can see:
+  
+# The first dimension (Can1) separates pH 7.6 from the other two treatments Can2 quite separates pH 7.8 from 8.0
 
 
+str(can)
+
+ellipse_df <- can$means %>%
+  data.frame() %>%
+  rownames_to_column(var= 'pH')
+
+data.frame(can$structure+can$coeffs.std ) %>%
+  rownames_to_column(var= 'hpf') -> segment_df
+
+can$scores %>% 
+  mutate(pH = factor(pH, levels = rev(names(pHpalette))),
+    g = 'Growth') %>% as_tibble() -> scores_df
+
+ggplot() + 
+  geom_hline(yintercept = 0, colour = "grey50", linetype = 2) +
+  geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
+  geom_point(data = scores_df, 
+    aes(x = Can1,y = Can2, color = pH), alpha = 0.5) +
+  scale_color_manual(values = pHpalette) +
+  scale_fill_manual(values = pHpalette) +
+  ggforce::geom_mark_ellipse(
+    data = ellipse_df, aes(Can1,y = Can2, fill = pH), 
+    color = NA) +
+  theme_classic(base_size = 16, base_family = "GillSans") +
+  geom_segment(data = segment_df, 
+    aes(x = 0, y = 0, xend = Can1, yend = Can2)) +
+  geom_text(data = segment_df, 
+    aes(x = Can1,y = Can2, label = hpf), 
+    family = "GillSans") +
+  coord_fixed() +
+  labs(x ='', y = '') +
+  theme(panel.border = element_blank(), legend.position = 'top')  -> p1
+
+p1  + facet_grid(~ g) + theme(
+  strip.background = element_rect(fill = 'grey', color = 'white')) -> p1
+
+ggsavepath <- paste0(getwd(), '/Figures/')
+
+
+ggsave(p1, path = ggsavepath, filename = 'candisc_growth.png',width = 4,height = 4, dpi = 300)
+
+# Calcification candisc
+
+mod <- lm(cbind(`48`,`60`,`108`) ~ pH , data=df1)
+
+
+can <- candisc(mod, data = df1, term = 'pH', ndim = "2")
+
+ellipse_df <- can$means %>%
+  data.frame() %>%
+  rownames_to_column(var= 'pH')
+
+data.frame(can$structure+can$coeffs.std ) %>%
+  rownames_to_column(var= 'hpf') -> segment_df
+
+can$scores %>% 
+  mutate(pH = factor(pH, levels = rev(names(pHpalette))),
+    g = 'Calcification') %>% as_tibble() -> scores_df
+
+ggplot() + 
+  geom_hline(yintercept = 0, colour = "grey50", linetype = 2) +
+  geom_vline(xintercept = 0, colour = "grey50", linetype = 2) +
+  geom_point(data = scores_df, 
+    aes(x = Can1,y = Can2, color = pH), alpha = 0.5) +
+  scale_color_manual(values = pHpalette) +
+  scale_fill_manual(values = pHpalette) +
+  ggforce::geom_mark_ellipse(
+    data = ellipse_df, aes(Can1,y = Can2, fill = pH), 
+    color = NA) +
+  theme_classic(base_size = 16, base_family = "GillSans") +
+  geom_segment(data = segment_df, 
+    aes(x = 0, y = 0, xend = Can1, yend = Can2)) +
+  geom_text(data = segment_df, 
+    aes(x = Can1,y = Can2, label = hpf), 
+    family = "GillSans") +
+  coord_fixed() +
+  labs(x ='', y = '') +
+  theme(panel.border = element_blank(), legend.position = 'top')  -> p1
+
+p1  + facet_grid(~ g) + theme(
+  strip.background = element_rect(fill = 'grey', color = 'white')) -> p1
+
+ggsavepath <- paste0(getwd(), '/Figures/')
+
+
+ggsave(p1, path = ggsavepath, filename = 'candisc_calcification.png',width = 4,height = 4, dpi = 300)
